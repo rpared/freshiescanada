663265f286f8ccf1b89df4e70882acaddea532ad
Roger Paredes <115596517+rpared@users.noreply.github.com>
2023-07-02 22:35:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let mainSlider = document.getElementsByClassName(".slider");
+let hamburger = document.getElementById();
+let menu = "loaded";
+
+function respMenu(){
+    console.log("function triggered");
+
+    document.querySelector("#slider").style.marginTop = "9em";
+   
+    
+}
