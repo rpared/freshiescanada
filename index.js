b4604301e5de2ddc6db8415a3dcdba3d8004f7ce
Roger Paredes <115596517+rpared@users.noreply.github.com>
2023-07-03 16:05:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let mainSlider = document.getElementsByClassName(".slider");
-let hamburger = document.getElementById();
-let menu = "loaded";
+let menu = false; 
 
 function respMenu(){
+    if (menu == false){
     console.log("function triggered");
-
     document.querySelector("#slider").style.marginTop = "9em";
-   
-    
+    menu = true;
+    }
+    else {
+        console.log("function 2 triggered");
+        document.querySelector("#slider").style.marginTop = "0";
+        menu = false;
+    }; 
 }
