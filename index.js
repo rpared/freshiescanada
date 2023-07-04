1740eb20ff0d14c760757a164b29135dd43d94a6
Roger Paredes <115596517+rpared@users.noreply.github.com>
2023-07-04 10:27:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-let menu = false; 
-
+let navMenu = false; 
+// Function to change the slider margin on click
 function respMenu(){
-    if (menu == false){
+    
+    if (navMenu == false){
     console.log("function triggered");
     document.querySelector("#slider").style.marginTop = "9em";
-    menu = true;
+    navMenu = true;
+    
     }
     else {
         console.log("function 2 triggered");
         document.querySelector("#slider").style.marginTop = "0";
-        menu = false;
+        navMenu = false;
     }; 
 }
+// Function to change the slider margin on media query of 600px
+function sliderPosition(x) {
+    if (x.matches) { // If media query matches
+        document.querySelector("#slider").style.marginTop = "0";
+    }
+  }
+  
+  let x = window.matchMedia("(min-width: 600px)")
+  sliderPosition(x) // Invokes function
+  x.addListener(sliderPosition) // Attach listener function on state changes > Don't understand this yet
