e75450a3abc7e30fe39d8bda1885934097d7c6dc
Roger Paredes <115596517+rpared@users.noreply.github.com>
2023-07-12 22:37:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,36 @@ function sliderPosition(x) {
   let x = window.matchMedia("(min-width: 800px)")
   sliderPosition(x) // Invokes function
   x.addListener(sliderPosition) // Attach listener function on state changes
+
+
+  // Steps pre Arrival
+  let stepsPreDisplay = false;
+  let stepsPreTitle = document.getElementById("Steps-btn1").innerHTML;
+  let stepsPre = "<ol><li>English Proficiency Exam</li><li>College Letter of Acceptance</li> <li>Digital Study permit</li><li>Get Flight Tickets</li><li>Reserve Accommodation</li><li>Prepare Luggage</li></ol>";
+
+  function getStepsPre(){
+    if (stepsPreDisplay == false){
+    document.getElementById("Steps-btn1").innerHTML = stepsPre;
+    stepsPreDisplay = true;
+    }
+    else if (stepsPreDisplay == true){
+        document.getElementById("Steps-btn1").innerHTML = stepsPreTitle;
+        stepsPreDisplay = false;
+        }
+      }
+
+   // Steps post Arrival
+   let stepsPostDisplay = false;
+   let stepsPostTitle = document.getElementById("Steps-btn2").innerHTML;
+   let stepsPost = "<ol><li>Retrieve physical Study Permit</li> <li>Get SIN number</li><li>Get canadian Sim Card</li><li>Open bank Account</li><li>Get TTC Presto card</li><li>Long Term Accommodation</li</ol>";
+
+  function getStepsPost(){
+    if (stepsPostDisplay == false){
+    document.getElementById("Steps-btn2").innerHTML = stepsPost;
+    stepsPostDisplay = true;
+    }
+    else if (stepsPostDisplay == true){
+    document.getElementById("Steps-btn2").innerHTML = stepsPostTitle;
+    stepsPostDisplay = false;
+    }
+  }
