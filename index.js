7ba5c89e5606f25ea938f0301b6bb4ba13c38719
Roger Paredes <115596517+rpared@users.noreply.github.com>
2023-07-23 20:41:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
-// Nav Bar
+
+// NAVBAR------------------------------
 let navMenu = false; 
+
 // Function to change the slider margin on click
-function respMenu(){
-    
+  function respMenu(){
+      
     if (navMenu == false){
-    console.log("function triggered");
-    document.getElementById("slider").style.marginTop = "9em";
+    console.log("Hamburger btn function triggered");
+    document.querySelector(".menu").style.display = "flex";
+    document.querySelector(".slider").style.top = "160px";
+    document.querySelector(".steps").style.marginTop = "160px";
+    // document.querySelector(".background").style.top = "80px";
     navMenu = true;
     
     }
-    else {
-        console.log("function 2 triggered");
-        document.getElementById("slider").style.marginTop = "0";
+    else if (navMenu == true){
+        console.log("Hamburger btn function 2 triggered");
+        document.querySelector(".menu").style.display = "none";
+        document.querySelector(".slider").style.top = "0";
+        document.querySelector(".steps").style.marginTop = "10px";
+        // document.querySelector(".background").style.top = "0";
         navMenu = false;
     }; 
-}
-// Function to change the slider margin on media query of 800px
-function sliderPosition(x) {
-    if (x.matches) { // If media query matches
-        document.getElementById("slider").style.marginTop = "0";
+  }
+
+
+// Function to change the screen margin on screen width > 800px
+  function screenWidth(x) {
+      if (x.matches) { // If media query matches
+        console.log("screenWidth ABOVE 800px function triggered"); 
         
+        document.querySelector(".menu").style.display = "block";
+        document.querySelector(".slider").style.top = "0";
+        document.querySelector(".steps").style.marginTop = "30px";
+        document.querySelector(".background").style.marginTop = "0";   
+      }
+    }
+    // Don't fully understand this, but the if clause made it work
+    let x = window.matchMedia("(min-width: 800px)")
+    if (x >= window.matchMedia("(width: 800px)")){
+      screenWidth(x); // Invokes function
+      x.addListener(screenWidth); // Attach listener function on state changes
+      // myInterval = setInterval(setSlider, 4500);
+    }
+
+// Function to change the screen margin on screen width < 800px
+  function screenWidth2(x1) {
+    if (x1.matches) { // If media query matches
+      console.log("screenWidth BELOW 800px function triggered"); 
+      console.log(x1)
+      document.querySelector(".menu").style.display = "none";
+      document.querySelector(".slider").style.top = "0";
+      document.querySelector(".steps").style.marginTop = "10px";
+      document.querySelector(".background").style.marginTop = "0";
+      navMenu = false;
     }
   }
-  // Don't fully understad this
-  let x = window.matchMedia("(min-width: 800px)")
-  sliderPosition(x) // Invokes function
-  x.addListener(sliderPosition) // Attach listener function on state changes
 
+  let x1 = window.matchMedia("(max-width: 799px)")
+  if (x1 <= window.matchMedia("(width: 799px)") ){
+    screenWidth2(x1); // Invokes function
+    x1.addListener(screenWidth2); // Attach listener function on state changes
+    // myInterval = setInterval(setSlider, 4500);
+  }
 
+// STEPS------------------------------
 // Steps pre Arrival
   let stepsPreDisplay = false;
   let stepsPreTitle = document.getElementById("Steps-btn1").innerHTML;
-  let stepsPre = "<ol><li>English Proficiency Exam</li><li>College Letter of Acceptance</li> <li>Digital Study Permit</li><li>Get Flight Tickets</li><li>Reserve Accommodation</li><li>Prepare Luggage</li></ol>";
+  let stepsPre = "<ol><li>English Proficiency Exam</li><li>College Letter of Acceptance</li> <li>Digital Study Permit</li><li>Get Flight Tickets</li><li>Reserve Accommodation</li></ol>";
 
   function getStepsPre(){
     if (stepsPreDisplay == false){
@@ -41,13 +78,14 @@ function sliderPosition(x) {
     else if (stepsPreDisplay == true){
         document.getElementById("Steps-btn1").innerHTML = stepsPreTitle;
         stepsPreDisplay = false;
+        
         }
       }
 
-   // Steps post Arrival
+// Steps post Arrival
    let stepsPostDisplay = false;
    let stepsPostTitle = document.getElementById("Steps-btn2").innerHTML;
-   let stepsPost = "<ol><li>Retrieve Physical Study Permit</li> <li>Get SIN Number</li><li>Get Canadian Sim Card</li><li>Open Bank Account</li><li>Get TTC Presto Card</li><li>Long Term Accommodation</li</ol>";
+   let stepsPost = "<ol><li>Retrieve Physical Study Permit</li> <li>Get SIN Number</li><li>Get Canadian Sim Card</li><li>Get TTC Presto Card</li><li>Long Term Accommodation</li</ol>";
 
   function getStepsPost(){
     if (stepsPostDisplay == false){
@@ -60,26 +98,41 @@ function sliderPosition(x) {
     }
   }
 
-//Home Slider
 
+// SLIDER------------------------------
+//Slider Variables:
+let slider1 = `
+    <a href="#stepsanchor" class="sliderbutton">
+    <div class="slidertext1"><img class="arrow" src="images/largearrow.png" alt=""> <p>Check the <span>Steps</span> before and after landing!</p></div>
+    <img class="background" src="images/Slider1-steps.jpg" alt="Photo of student at airport" title="Check the Steps pre and after landing!">             
+    </a>`;
 
-  //Slider Variables:
-  let slider1 = `<div class="slidertext1"><img class="arrow" src="images/largearrow.png" alt=""> <p>Check the <span>Steps</span> before and after landing!</p></div>     
-  <img class="background" src="images/Slider1-steps.jpg" alt="Photo of student at airport">    
-  </div>`;
+let slider2 = `
+    <a href="documentation.html" class="sliderbutton">
+    <div class="slidertext1"><img class="arrow" src="images/largearrow.png" alt=""> <p>Review the <span>Documents</span> you need!</p></div>
+    <img class="background" src="images/Slider2-documents.jpg" alt="Photo of documents">
+    </a>`;
 
-  let slider2 = `<div class="slidertext1"><img class="arrow" src="images/largearrow.png" alt=""> <p>Review the <span>Documents</span> you need!</p></div>
-  <img class="background" src="images/Slider2-documents.jpg" alt="Photo of documents">    
-  </div>`;
+let slider3 = `
+    <a href="transportation.html" class="sliderbutton"><div class="slidertext1"><img class="arrow" src="images/largearrow.png" alt=""> <p>Handle <span>Transportation</span> like a pro commuter!</p></div>
+    <img class="background" src="images/Slider3-transportation.jpg" alt="Photo of Toronto Street Car">
+    </a>`;
 
-  let slider3 = `<div class="slidertext1"><img class="arrow" src="images/largearrow.png" alt=""> <p>Handle <span>Transportation</span> like a pro commuter!</p></div>
-  <img class="background" src="images/Slider3-transportation.jpg" alt="Photo of Toronto Street Car">    
-  </div>`;
-
-
-  function respMenuJs(){
-    console.log("hamburger function triggered");
-    document.querySelector(".menu").style.display = "flex";
-    document.querySelector("#menu").style.position = "relative";
-
+myInterval = setInterval(setSlider, 4500);
+let slider = document.querySelector(".slider");
+slider.innerHTML = slider1;
+ 
+// Slider Function
+function setSlider() {
+  console.log("setSlider function triggered");
+  if (slider.innerHTML == slider1){
+    slider.innerHTML = slider2;
   }
+  else if (slider.innerHTML == slider2){
+    slider.innerHTML = slider3;
+  }
+  else if (slider.innerHTML == slider3){
+    slider.innerHTML = slider1;
+  }
+}
+  
--- services.js
+++ services.js
@@ -3,24 +3,45 @@ let navMenu = false;
 function respMenu(){
     
     if (navMenu == false){
-    console.log("function triggered");
-    document.querySelector(".banner").style.marginTop = "10em";
-    navMenu = true;
+        console.log("Hamburger btn function triggered");
+        document.querySelector(".menu").style.display = "flex";
+        navMenu = true;
     
     }
     else {
-        console.log("function 2 triggered");
-        document.querySelector(".banner").style.marginTop = "0";
+        console.log("Hamburger btn function 2 triggered");
+        document.querySelector(".menu").style.display = "none";
         navMenu = false;
     }; 
 }
-// Function to change the slider margin on media query of 800px
-function sliderPosition(x) {
+// Function to change the screen margin on screen width > 800px
+function screenWidth(x) {
     if (x.matches) { // If media query matches
-        document.querySelector(".banner").style.marginTop = "0";
+      console.log("screenWidth ABOVE 800px function triggered"); 
+      document.querySelector(".menu").style.display = "block";
     }
   }
-  // Don't fully understad this
+  // Don't fully understand this, but the if clause made it work
   let x = window.matchMedia("(min-width: 800px)")
-  sliderPosition(x) // Invokes function
-  x.addListener(sliderPosition) // Attach listener function on state changes
+  if (x >= window.matchMedia("(width: 800px)")){
+    screenWidth(x); // Invokes function
+    x.addListener(screenWidth); // Attach listener function on state changes
+
+  }
+
+// Function to change the screen margin on screen width < 800px
+function screenWidth2(x1) {
+  if (x1.matches) { // If media query matches
+    console.log("screenWidth BELOW 800px function triggered"); 
+    console.log(x1)
+    document.querySelector(".menu").style.display = "none";
+    navMenu = false;
+  }
+}
+
+let x1 = window.matchMedia("(max-width: 799px)")
+if (x1 <= window.matchMedia("(width: 799px)") ){
+  screenWidth2(x1); // Invokes function
+  x1.addListener(screenWidth2); // Attach listener function on state changes
+  // myInterval = setInterval(setSlider, 4500);
+}
